d35819f33ee4a004156f172fa831bae5f13b6ae0
Saurabh Kulkarni <kulkarnis009@gmail.com>
2022-08-22 16:24:32 +0530

    new

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const express = require("express")
 const path = require("path")
 const app = express()
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 // const bodyparser = require('body-parser')
-mongoose.connect('mongodb://localhost/contactDance', {useNewUrlParser: true});
+// mongoose.connect('mongodb://localhost/contactDance', {useNewUrlParser: true});
 const port = 80
 
 //define mongoose schema
-const contactSchema = new mongoose.Schema({
-    name: String,
-    phone: String,
-    email: String,
-    address: String,
-    desc: String,
-  });
+// const contactSchema = new mongoose.Schema({
+//     name: String,
+//     phone: String,
+//     email: String,
+//     address: String,
+//     desc: String,
+//   });
 
-  const Contact = mongoose.model('Contact', contactSchema);
+//   const Contact = mongoose.model('Contact', contactSchema);
 
 app.use('/static', express.static('static'))
 app.use(express.urlencoded())
@@ -32,13 +32,13 @@ app.get('/contact',(req,res)=>{
     res.status(200).render('contact.pug', params)
 })
 app.post('/contact',(req,res)=>{
-    var myData = new Contact(req.body)
-    myData.save().then(()=>{
-        // res.status(200).send("Item was saved to the database")
-    }).catch(()=>{
-        res.status(400).send("Item was not saved to the database")
-    });
-    res.status(200).render('contact.pug');
+    // var myData = new Contact(req.body)
+    // myData.save().then(()=>{
+    //     // res.status(200).send("Item was saved to the database")
+    // }).catch(()=>{
+    //     res.status(400).send("Item was not saved to the database")
+    // });
+    // res.status(200).render('contact.pug');
     
 })
 app.get('/about',(req,res)=>{
