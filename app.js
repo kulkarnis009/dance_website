9bacbfe44737a966f100b35163c2251225223bae
Saurabh Kulkarni <kulkarnis009@gmail.com>
2022-08-23 12:37:30 +0530

    new

--- app.js
+++ app.js
@@ -2,26 +2,26 @@ const express = require("express")
 const path = require("path")
 const app = express()
 const http = require('http')
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 // const bodyparser = require('body-parser')
-// mongoose.connect('mongodb://localhost/contactDance', {useNewUrlParser: true});
-const port = 80
+mongoose.connect('mongodb://localhost/contactDance', {useNewUrlParser: true});
+const port =  process.env.PORT || 80
 
 // const app = http.createServer((req,res) =>{
 //     res.writeHead(200, {"Content-Type": "text/plain"});
 //     res.end("it works");
 // })
 
-//define mongoose schema
-// const contactSchema = new mongoose.Schema({
-//     name: String,
-//     phone: String,
-//     email: String,
-//     address: String,
-//     desc: String,
-//   });
+// define mongoose schema
+const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String,
+  });
 
-//   const Contact = mongoose.model('Contact', contactSchema);
+  const Contact = mongoose.model('Contact', contactSchema);
 
 app.use('/static', express.static('static'))
 app.use(express.urlencoded())
@@ -38,13 +38,13 @@ app.get('/contact',(req,res)=>{
     res.status(200).render('contact.pug', params)
 })
 app.post('/contact',(req,res)=>{
-    // var myData = new Contact(req.body)
-    // myData.save().then(()=>{
-    //     // res.status(200).send("Item was saved to the database")
-    // }).catch(()=>{
-    //     res.status(400).send("Item was not saved to the database")
-    // });
-    // res.status(200).render('contact.pug');
+    var myData = new Contact(req.body)
+    myData.save().then(()=>{
+        // res.status(200).send("Item was saved to the database")
+    }).catch(()=>{
+        res.status(400).send("Item was not saved to the database")
+    });
+    res.status(200).render('contact.pug');
     
 })
 app.get('/about',(req,res)=>{
